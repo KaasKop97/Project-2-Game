a894bfda5ffccc99a406b5a3f0d472fa98232a39
Charone1 <31688853+Charone1@users.noreply.github.com>
2018-01-10 17:59:13 +0100

    game

---
+++ GAME/games/dodge_fangirls/enemy.py
@@ -0,0 +1,24 @@
+import pygame
+from handlers import config_handler
+
+class Enemys(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, img, score):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.X = x
+        self.Y = y
+        self.W = w
+        self.H = h
+        self.speed = 10
+        self.score = score
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X
+        self.rect.y = self.Y
+        self.conf = config_handler.ConfigHandler()
+        self.game_width = int(self.conf.get_value("game", "width"))
+        self.game_height = int(self.conf.get_value("game", "height"))
+
+    def update(self):
+        print(self.rect.y)
+        self.rect.move_ip(0, self.speed)
---
+++ GAME/games/dodge_fangirls/player.py
@@ -0,0 +1,25 @@
+import pygame
+from handlers import config_handler
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, img):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.X = x
+        self.Y = y
+        self.W = w
+        self.H = h
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X
+        self.rect.y = self.Y
+        self.conf = config_handler.ConfigHandler()
+        self.game_width = int(self.conf.get_value("game", "width"))
+        self.game_height = int(self.conf.get_value("game", "height"))
+        self.changeX = 0
+
+    def update(self):
+
+
+        self.rect.move_ip(self.changeX, 0)
