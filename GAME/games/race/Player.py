c4a91de62e0b330d131c7651cc84cc7ee6f6e035
amanpreet17 <31688916+amanpreet17@users.noreply.github.com>
2018-01-12 21:16:11 +0100

    update
    update

--- GAME/games/race/Player.py
+++ GAME/games/race/Player.py
@@ -20,7 +20,7 @@ class Player (pygame.sprite.Sprite):
         self.game_width         =       int(self.conf.get_value("game", "width"))
         self.game_height        =       int(self.conf.get_value("game", "height"))
         self.changeX            =       0
-        self.dead               =       True
+        self.dead               =       False
 
     def update(self):
 
