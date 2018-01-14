a307b5248aa7c5d803131f188991c7652cc7b598
mitch <mitchelvanhamburg@gmail.com>
2018-01-14 21:41:39 +0100

    Fix lagg i hope

--- GAME/main.py
+++ GAME/main.py
@@ -23,45 +23,57 @@ class Main:
         self.bg_rect = self.background.get_rect()
         self.clock = pygame.time.Clock()
 
-        self.misc = misc_helper.MiscHelper()
         self.menu = menu_helper.MenuHelper(self.background)
         self.game_handler = game_handler.GameHandler(self.background)
 
     def main_loop(self):
-        #menu_to_draw = ""
-        self.menu.draw_menu("main")
+        menu_to_draw = "main"
+        self.menu.draw_menu(menu_to_draw)
         while self.running:
             self.clock.tick(60)
+            if not menu_to_draw == "":
+                self.menu.draw_menu(menu_to_draw)
             if not self.game_handler.loaded_game:
                 pygame.mouse.set_visible(True)
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         button_pressed = self.menu.get_button_pressed(event.pos)
                         if not button_pressed == self.menu.currently_drawn:
-                            if button_pressed == "PLAY":
-                                #menu_to_draw = "PLAY"
-                                self.menu.draw_menu("PLAY")
+                            if button_pressed == "PLAY" and not menu_to_draw == "PLAY":
+                                menu_to_draw = "PLAY"
+                                #self.menu.draw_menu("PLAY")
                             elif button_pressed == "QUIT":
                                 self.running = False
                                 pygame.quit()
                             elif button_pressed == "DRON":
                                 self.game_handler.load_game("DRON")
+                                menu_to_draw = ""
+                                self.menu.reset_menu()
+                                self.menu = None
                             elif button_pressed == "Dodge the Fangirls":
                                 self.game_handler.load_game("Dodge the Fangirls")
+                                menu_to_draw = ""
+                                self.menu.reset_menu()
+                                self.menu = None
                             elif button_pressed == "Race":
                                 self.game_handler.load_game("Race")
+                                menu_to_draw = ""
+                                self.menu.reset_menu()
+                                self.menu = None
                             elif button_pressed == "Galaxy Trespassers":
                                 self.game_handler.load_game("Galaxy Trespassers")
+                                menu_to_draw = ""
+                                self.menu.reset_menu()
+                                self.menu = None
                             else:
                                 print("ERROR: Can't load button since there is no definition for it.")
                     elif event.type == pygame.QUIT:
                         print("Quitting")
                         self.running = False
-                        pygame.quit()
-                #self.menu.get_hover(pygame.mouse.get_pos())
+                if self.menu:
+                    self.menu.get_hover(pygame.mouse.get_pos())
             else:
                 pygame.mouse.set_visible(False)
-                self.menu.reset_menu()
                 self.game_handler.main_loop()
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN:
@@ -72,8 +84,9 @@ class Main:
                         self.game_handler.key_down(event.key)
                         if event.key == 27:
                             self.game_handler.unload_game()
-                            #menu_to_draw = "main"
-                            self.menu.draw_menu("main")
+                            menu_to_draw = "main"
+                            self.menu = menu_helper.MenuHelper(self.background)
+                            #self.menu.draw_menu("main")
                     elif event.type == pygame.KEYUP:
                         self.game_handler.key_up(event.key)
                     elif event.type == pygame.QUIT:
